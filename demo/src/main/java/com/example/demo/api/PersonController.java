94831454dced8eb8030072bbebad800d86206369
ongcj <chengjie13@hotmail.com>
2020-12-16 15:19:22 +0800

    added functionalilty for retrieving list of person

--- demo/src/main/java/com/example/demo/api/PersonController.java
+++ demo/src/main/java/com/example/demo/api/PersonController.java
@@ -3,10 +3,9 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("api/v1/person")
 @RestController // this is a RestController class, where we can have methods and expose some endpoints that client can consume.
@@ -20,5 +19,9 @@ public class PersonController {
     @PostMapping // tell spring that this method will be used as a POST request.
     public void addPerson(@RequestBody Person person) { // take the request body and put inside Person
         personService.addPerson(person);
+    }
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
     }
 }
